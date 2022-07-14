0d7ecbb5478d9001896be051d42db5b83a0730ef
Covali Andrei <krab4ek@gmail.com>
2022-07-14 20:33:29 +0300

    Add project files.

---
+++ covali.InterfaceNameClash/IDrawToMemory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace covali.InterfaceNameClash
+{
+    internal interface IDrawToMemory
+
+    {
+        void Draw();
+    }
+}
