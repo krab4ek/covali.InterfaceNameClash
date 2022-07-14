eca30ec1b6da7d43165a551062c14362b9173066
Covali Andrei <krab4ek@gmail.com>
2022-07-14 21:11:13 +0300

    update

---
+++ covali.InterfaceNameClash/Octagon.cs
@@ -0,0 +1,23 @@
+﻿namespace covali.InterfaceNameClash
+{
+    internal class Octagon : IDrawToForm, IDrawToMemory, IDrawToPrinter
+    {
+        void IDrawToForm.Draw()
+        {
+            Console.WriteLine("Drawing to form...");
+        }
+        void IDrawToMemory.Draw()
+        {
+            Console.WriteLine("Drawing to memory...");
+        }
+        void IDrawToPrinter.Draw()
+        {
+            Console.WriteLine("Drawing to printer...");
+        }
+
+        //public void Draw()
+        //{
+        //    Console.WriteLine("Drawing the Octagon....");
+        //}
+    }
+}
--- covali.InterfaceNameClash/Program.cs
+++ covali.InterfaceNameClash/Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿using covali.InterfaceNameClash;
+
+Console.WriteLine("***** Fun with Interface Name Clashes *****\n");
+
+Octagon oct = new();
+IDrawToForm itfForm = (IDrawToForm)oct;
+itfForm.Draw();
+
+IDrawToMemory itfMemory = (IDrawToMemory)oct;
+itfMemory.Draw();
+
+IDrawToPrinter itfPrinter = (IDrawToPrinter)oct;
+itfPrinter.Draw();
+
+
+
+
+
+
+Console.ReadLine();
